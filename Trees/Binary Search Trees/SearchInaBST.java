0669e7d185d06d7d1bd75ec2e0394f1c937b3ac6
DHRUV SHARMA <dsharma.gzbd@gmail.com>
2025-07-07 23:24:40 +0530

    Search in a BST

---
+++ Trees/Binary Search Trees/SearchInaBST.java
@@ -0,0 +1,50 @@
+public class SearchInaBST {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+
+        }
+    }
+    public static boolean Search(Node root, int key) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.data == key) {
+            return true;
+        }
+
+        if (root.data > key) {
+            return Search(root.left, key);
+        } else {
+            return Search(root.right, key);
+        }
+    }
+    
+    public static void main(String args[]) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        // 1
+        // / \
+        // 2 3
+        // / \ / \
+        // 4 5 6 7
+
+        // Assign references to the nodes you want to pass
+        Node p = root.left.left; // Node with value 4
+        Node q = root.left.right; // Node with value 5
+
+        System.out.println(Search(root, 7));
+      
+    }
+}
