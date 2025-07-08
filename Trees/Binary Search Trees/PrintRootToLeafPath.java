058bd8b70a400992ded302dbbcf3ff3f90411618
DHRUV SHARMA <dsharma.gzbd@gmail.com>
2025-07-08 20:10:20 +0530

    Root to leaf path in a tree

---
+++ Trees/Binary Search Trees/PrintRootToLeafPath.java
@@ -0,0 +1,56 @@
+import java.util.*;
+public class PrintRootToLeafPath {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+
+        }
+    }
+    
+    public static void PrintPath(ArrayList<Integer> path) {
+        for (int i = 0; i < path.size(); i++) {
+            System.out.print(path.get(i) + "->");
+        }
+        System.out.println("null");
+    }
+    public static void Root2LeafPath(ArrayList<Integer> path, Node root){
+        if(root==null){
+            return;
+        }
+        path.add(root.data);
+        if(root.left==null && root.right == null){
+            PrintPath(path);
+        }
+
+        Root2LeafPath(path, root.left);
+        Root2LeafPath(path, root.right);
+        path.remove(path.size()-1);
+    }
+    
+    public static void main(String args[]) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        // 1
+        // / \
+        // 2 3
+        // / \ / \
+        // 4 5 6 7
+
+        // Assign references to the nodes you want to pass
+        Node p = root.left.left; // Node with value 4
+        Node q = root.left.right; // Node with value 5
+
+        System.out.println();
+
+    }
+}
